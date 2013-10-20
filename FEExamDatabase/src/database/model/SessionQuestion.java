cabd423cf5967fb397897247f2a4bbcb57848071
unknown <Huynh Quang Thao@ThaoHQSE60963.(none)>
2013-10-20 13:22:02 +0700

    Add new Database DAL

---
+++ FEExamDatabase/src/database/model/SessionQuestion.java
@@ -0,0 +1,57 @@
+package database.model;
+
+public class SessionQuestion {
+
+	int sessionQuestionId;
+	int sessionId;
+	String categoryName;
+	int idQuestion;
+	int percentCorrect;
+	
+	public SessionQuestion(int sessionQuestionId, int sessionId, String categoryName, int idQuestion, int percentCorrect) {
+		super();
+		this.sessionQuestionId = sessionQuestionId;
+		this.sessionId = sessionId;
+		this.categoryName = categoryName;
+		this.idQuestion = idQuestion;
+		this.percentCorrect = percentCorrect;
+	}
+	
+	
+	public int getSessionQuestionId() {
+		return sessionQuestionId;
+	}
+
+
+	public void setSessionQuestionId(int sessionQuestionId) {
+		this.sessionQuestionId = sessionQuestionId;
+	}
+
+
+	public int getSessionId() {
+		return sessionId;
+	}
+	public void setSessionId(int sessionId) {
+		this.sessionId = sessionId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public int getIdQuestion() {
+		return idQuestion;
+	}
+	public void setIdQuestion(int idQuestion) {
+		this.idQuestion = idQuestion;
+	}
+	public int getPercentCorrect() {
+		return percentCorrect;
+	}
+	public void setPercentCorrect(int percentCorrect) {
+		this.percentCorrect = percentCorrect;
+	}
+	
+	
+}
